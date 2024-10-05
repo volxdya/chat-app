35f5df54a7de525423b4b94fee4b5e20c23b28a9
volxdya <recommendation082@gmail.com>
2024-10-05 23:49:09 +0500

    some fixed get limit messages

--- src/modules/messages/messages.service.ts
+++ src/modules/messages/messages.service.ts
@@ -15,8 +15,16 @@ export class MessagesService {
     return this.messagesModel.findAll({ include: { all: true } });
   }
 
+  // Отдаем последние три созданных сообщения, запрос нужен для прогузки первых 3 сообщений, дальше по сокетам 
   async getLastMessages(chatId: number) {
-    const messages: Message[] = await this.messagesModel.findAll({ where: { chatId } });
+    const messages: Message[] = await this.messagesModel.findAll(
+      {
+        limit: 2,
+        order: ['createdAt'],
+        offset: (await this.messagesModel.count()) - 3,
+        where: { chatId }
+      }
+    );
 
     return messages;
   }
